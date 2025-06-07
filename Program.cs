e8bbee6f6711b00123faa74c25ceebbe6c917ae6
Choko299 <mainepic29@gmail.com>
2025-06-08 01:49:44 +0400

    Create Program.cs
    ლეპტოპის DataBase - აპლიაკაცია

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Windows.Forms;
+using LaptopManager;
+
+namespace WinFormsApp1
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
